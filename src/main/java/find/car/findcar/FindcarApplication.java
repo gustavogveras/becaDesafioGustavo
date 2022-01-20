830e6dbc5dde820078a403049d1c129ad5bfe3bd
Gustavo Noleto Veras <gnoletov@emeal.nttdata.com>
2022-01-19 23:33:40 -0300

    adicionando arquivos iniciais

---
+++ src/main/java/find/car/findcar/FindcarApplication.java
@@ -0,0 +1,13 @@
+package find.car.findcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindcarApplication.class, args);
+	}
+
+}
